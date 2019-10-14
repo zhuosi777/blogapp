b4d576ca06a60ccc7e1f284ccf3399509141dc58
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-14 18:38:58 +0900

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all.order(created_at: :DESC)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice]="投稿を作成しました"
+    redirect_to("/posts/index")
+    else 
+      render("posts/new")
+    end
+  end
+
+  def edit
+  @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+
+    if @post.save
+    flash[:notice] = "投稿を編集しました"
+    redirect_to("/posts/index")
+    else
+      render("posts/edit")
+  end
+end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice]="投稿を削除しました"
+    redirect_to("/posts/index")
+  end
+end
+
